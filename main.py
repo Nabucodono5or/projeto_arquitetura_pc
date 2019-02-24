07861aecfe9201a04dcfcfff34fbc9a33b09ea49
Jefferson Abreu <pereirajefferson06@gmail.com>
2019-02-23 23:02:10 -0300

    informações de consumo e do processador impressas

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 from monitoramento import cpu
 
+tempo = int(input("Entrada de tempo: "))
 print('Processador: ', cpu.nome_cpu())
-print(cpu.frequencia())
+print('Velocidade: {} GHz'.format(cpu.frequencia()))
+print('Cores : ', cpu.cores())
+print("Cores fìsicos: ", cpu.cores_fisicos())
+print("___________________")
+def imprimir_consumo(t):
+    for x in range(t):
+        print("Consumido: ", round(cpu.consumo_cpu_porcentagem_formatado(), 1), "%")
+        print("Livre :", round(cpu.cpu_livre_porcentagem()), "%")
+        print("_________________")
 
+if tempo >= 1:
+    imprimir_consumo(tempo)
--- monitoramento/cpu.py
+++ monitoramento/cpu.py
@@ -2,18 +2,22 @@ import psutil
 import platform
 import os
 
+
 # modulo de operaçoes para cheque da cpu
 
 def frequencia():
-    return round((psutil.cpu_freq().max/1000), 1)
+    return psutil.cpu_freq().max / 1000
+
 
 def nome_cpu():
     if sistema_operacional() == 'Linux':
         return busca_info_arq()
     return platform.processor()
 
+
 def sistema_operacional():
     return os.uname().sysname
+
 
 def busca_info_arq():
     with open('/proc/cpuinfo') as f:
@@ -24,4 +28,25 @@ def busca_info_arq():
                     model = model_name
                     model = model.strip()
                     break
-    return model
+    return model + " " + platform.processor()
+
+
+def cores():
+    return psutil.cpu_count()
+
+
+def cores_fisicos():
+    return psutil.cpu_count(logical=False)
+
+
+def consumo_cpu_percentagem():
+    return psutil.cpu_times_percent(interval=1)
+
+
+def consumo_cpu_porcentagem_formatado():
+    consumo = consumo_cpu_percentagem()
+    return consumo.user + consumo.system
+
+
+def cpu_livre_porcentagem():
+    return consumo_cpu_percentagem().idle
