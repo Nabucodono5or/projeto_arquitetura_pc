f49de9e78f7ecd88ef353e1ccddb302f377ae207
Jefferson Abreu <pereirajefferson06@gmail.com>
2019-02-25 21:17:53 -0300

    criado modulo de monitoramento da memoria

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from monitoramento import cpu
+from monitoramento import memory
 
 tempo = int(input("Entrada de tempo: "))
 print('Processador: ', cpu.nome_cpu())
 print('Velocidade: {} GHz'.format(cpu.frequencia()))
 print('Cores : ', cpu.cores())
 print("Cores fìsicos: ", cpu.cores_fisicos())
+print()
 print("___________________")
+
 def imprimir_consumo(t):
     for x in range(t):
-        print("Consumido: ", round(cpu.consumo_cpu_porcentagem_formatado(), 1), "%")
-        print("Livre :", round(cpu.cpu_livre_porcentagem()), "%")
+        print("Consumo da CPU: ", round(cpu.consumo_cpu_porcentagem_formatado(), 1), "%")
+        # print("Livre :", round(cpu.cpu_livre_porcentagem()), "%")
+        print("Consumo da memória: ", memory.porcentagem(), "%")
         print("_________________")
 
 if tempo >= 1:
