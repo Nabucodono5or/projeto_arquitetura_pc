ddac5b1d311f18241ed08983e1f906a56d526d99
Jefferson Abreu <pereirajefferson06@gmail.com>
2019-03-02 22:08:11 -0300

    Terminada interface em linha de código responsabilidade de main v2.00

--- main.py
+++ main.py
@@ -9,46 +9,77 @@ from monitoramento import disks
 from monitoramento import rede
 
 
-# tempo = int(input("Entrada de tempo: "))
-print('Processador: ', cpu.nome_cpu())
-print('Arquitetura: ', cpu.arquitetura())
-print('Palavra: ', cpu.palavra(), 'bits')
-print('Velocidade: {} GHz'.format(cpu.frequencia()))
-print('Cores (físicos): ', cpu.cores(), '(', cpu.cores_fisicos(), ')')
-print("___________________")
+def imprimir_info_cpu():
+    print()
+    print("___________________")
+    print("CPU")
+    print('Processador: ', cpu.nome_cpu())
+    print('Arquitetura: ', cpu.arquitetura())
+    print('Palavra: ', cpu.palavra(), 'bits')
+    print('Velocidade: {} GHz'.format(cpu.frequencia()))
+    print('Cores (físicos): ', cpu.cores(), '(', cpu.cores_fisicos(), ')')
+    print("Consumo atual da CPU: ", round(cpu.consumo_cpu_porcentagem_formatado(), 1), "%")
+    print()
+    print("___________________")
+    print()
 
 
-print("Total  de memória: ", memory.total_gb(), "GB")
-print("Total de memória swap: ", memory.memoria_swap(), "GB")
-print("Memória swap usada: ", memory.memoria_swap_usada(), "GB")
-print("___________________")
-
-print("Total do disco: ", disks.total_gb(), "GB")
-print("Total de disco em uso: ", disks.usado_gb(), "GB")
-print("Total de espaço livre em disco: ", disks.livre_gb(), "GB")
-print("___________________")
-
-print("Endereço IP: ", rede.ip_adress())
-# print()
-print("___________________")
+def imprimir_info_memoria():
+    print()
+    print("___________________")
+    print("MEMÓRIA")
+    print("Total  de memória: ", memory.total_gb(), "GB")
+    print("Total de memória swap: ", memory.memoria_swap(), "GB")
+    print("Memória swap usada: ", memory.memoria_swap_usada(), "GB")
+    print("Consumo atual da memória: ", memory.porcentagem(), "%")
+    print("___________________")
+    print()
 
 
-# def imprimir_consumo(t):
-#     for x in range(t):
-#         print("Consumo atual da CPU: ", round(cpu.consumo_cpu_porcentagem_formatado(), 1), "%")
-#         # print("Livre :", round(cpu.cpu_livre_porcentagem()), "%")
-#         print("Consumo atual da memória: ", memory.porcentagem(), "%")
-#         print("Consumo atual do disco: ", disks.porcentagem_de_uso(), "%")
-#         print("_________________")
+def imprimir_info_disco():
+    print()
+    print("___________________")
+    print("DISCO")
+    print("Total do disco: ", disks.total_gb(), "GB")
+    print("Total de disco em uso: ", disks.usado_gb(), "GB")
+    print("Total de espaço livre em disco: ", disks.livre_gb(), "GB")
+    print("Consumo atual do disco: ", disks.porcentagem_de_uso(), "%")
+    print("___________________")
+    print()
+
+def imprimir_info_rede():
+    print()
+    print("___________________")
+    print("REDE")
+    print("Endereço IP: ", rede.ip_adress())
+    print("___________________")
+    print()
+
+def iniciar_app():
+    quit = False
+
+    while not quit:
+        print("MONITORAMENTO DE COMPUTADOR")
+        print(" --- escreva cpu para informações sobre cpu;")
+        print(" --- escreva disco para informações sobre disco;")
+        print(" --- escreva mem/memoria para informações sobre memória;")
+        print(" --- escreva ip para informações sobre Rede;")
+        print(" --- escreva resumo/t para informações resumidas;")
+        print(" --- Escreva q/quit para sair;")
+        print()
+        resultado = input("Entre com a informação que você queira: ")
+
+        if resultado == "cpu" :
+            imprimir_info_cpu()
+        elif resultado == "mem" or resultado == "memoria" :
+            imprimir_info_memoria()
+        elif resultado == "disco" :
+            imprimir_info_disco()
+        elif resultado == "ip" :
+            imprimir_info_rede()
+        elif resultado == "q" or "quit" :
+            quit = True
 
 
-# if tempo >= 1:
-#     imprimir_consumo(tempo)
-
-
-
-
-
-#
-# if __name__ == '__main__':
-#     iniciar_app()
+if __name__ == '__main__':
+    iniciar_app()
